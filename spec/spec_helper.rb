14325f7ba28dcf231626a05f18a1d490111c5901
Viacheslav Molokov <viacheslav.molokov@gmail.com>
2018-05-04 14:10:49 +0300

    Fix warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ require File.expand_path("../../lib/xml-normalizer", __FILE__)
 Dir[File.join(File.expand_path("../", __FILE__), "support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   # config.run_all_when_everything_filtered = false
   # config.filter_run :focus
   config.mock_with :rspec
